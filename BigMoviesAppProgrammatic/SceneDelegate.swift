bdca3786764a640aaa284fba6fc022d4d4347d04
Fatih <fatihozen247@gmail.com>
2023-06-26 17:57:21 +0300

    tabbar icons have been changed

--- BigMoviesAppProgrammatic/SceneDelegate.swift
+++ BigMoviesAppProgrammatic/SceneDelegate.swift
@@ -17,22 +17,32 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let tabBarController = UITabBarController()
         
-        let item1 = UITabBarItem(tabBarSystemItem: .mostViewed, tag: 0)
-        let item2 = UITabBarItem(tabBarSystemItem: .mostRecent, tag: 1)
-        let item3 = UITabBarItem(tabBarSystemItem: .search, tag: 2)
-        let item4 = UITabBarItem(tabBarSystemItem: .favorites, tag: 3)
+//        let item1 = UITabBarItem(tabBarSystemItem: .mostViewed, tag: 0)
+//        let item2 = UITabBarItem(tabBarSystemItem: .mostRecent, tag: 1)
+//        let item3 = UITabBarItem(tabBarSystemItem: .search, tag: 2)
+//        let item4 = UITabBarItem(tabBarSystemItem: .favorites, tag: 3)
         
-        let firstVC = UINavigationController(rootViewController: HomeScreen(ApiControl: "movie"))
-        let secondVC = UINavigationController(rootViewController: HomeScreen(ApiControl: "tv"))
-        let thirdVC = UINavigationController(rootViewController: SearchScreen())
-        let fourthVC = UINavigationController(rootViewController: FavoriteScreen())
+        let vc1 = UINavigationController(rootViewController: HomeScreen(ApiControl: "movie"))
+        let vc2 = UINavigationController(rootViewController: HomeScreen(ApiControl: "tv"))
+        let vc3 = UINavigationController(rootViewController: SearchScreen())
+        let vc4 = UINavigationController(rootViewController: FavoriteScreen())
         
-        firstVC.tabBarItem = item1
-        secondVC.tabBarItem = item2
-        thirdVC.tabBarItem = item3
-        fourthVC.tabBarItem = item4
+//        firstVC.tabBarItem = item1
+//        secondVC.tabBarItem = item2
+//        thirdVC.tabBarItem = item3
+//        fourthVC.tabBarItem = item4
+        vc1.tabBarItem.image = UIImage(systemName: "film")
+        vc1.tabBarItem.selectedImage = UIImage(systemName: "film.fill")
         
-        tabBarController.viewControllers = [firstVC,secondVC,thirdVC,fourthVC]
+        vc2.tabBarItem.image = UIImage(systemName: "tv")
+        vc2.tabBarItem.selectedImage = UIImage(systemName: "tv.fill")
+        
+        vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        
+        vc4.tabBarItem.image = UIImage(systemName: "star")
+        vc4.tabBarItem.selectedImage = UIImage(systemName: "star.fill")
+        
+        tabBarController.viewControllers = [vc1,vc2,vc3,vc4]
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
